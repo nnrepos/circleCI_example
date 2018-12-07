11065338d6d396ad788afd83dd2b511e7a56eca2
NN <niv_ssa@walla.com>
2018-12-07 14:17:54 +0200

    fixed tree structure

--- test_calc.py
+++ test_calc.py
@@ -1,13 +1,13 @@
-from calc import *
+import calc
 
 class Tester:
 
   def test_add(self):
-    assert 4 == add(1,3)
-    assert 5 == add(6,-1)
+    assert 4 == calc.add(1,3)
+    assert 5 == calc.add(6,-1)
   
   def test_sub(self):
-    assert 1 == sub(3,2);
+    assert 1 == calc.sub(3,2);
   
   def test_shout(self):
-    assert "DONUTS!!!" == shout('donuts')
+    assert "DONUTS!!!" == calc.shout('donuts')
